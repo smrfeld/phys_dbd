6d96628692e41024b37d4e2acce9bebc3e8b6cdf
Oliver K. Ernst <oernst@ucsd.edu>
2021-06-11 15:07:13 -0700

    model

--- playground/test.py
+++ playground/test.py
@@ -1,6 +1,8 @@
-from physPCA import ImportHelper, DataDesc, ParamsTraj
+from physPCA import ImportHelper, DataDesc, ParamsTraj, RxnSpec, RxnInputsLayer, ParamsTETraj
 
 import numpy as np
+
+import tensorflow as tf
 
 data_desc = DataDesc(
     no_seeds=50,
@@ -18,15 +20,70 @@ data = ImportHelper.import_gillespie_ssa_from_data_desc(
     ip3_dir="ip3_0p100"
 )
 
-muh = np.zeros(1)
-varh_diag = np.ones(1)
-params_traj = ParamsTraj.fromPCA(data, data_desc.times, muh, varh_diag)
+if False:
 
-# Export
-params_traj.export("cache_params.txt")
+    # Create params traj and export
+    muh = np.zeros(1)
+    varh_diag = np.ones(1)
+    params_traj = ParamsTraj.fromPCA(data, data_desc.times, muh, varh_diag)
 
-# Differentiate
-paramsTE_traj = params_traj.differentiate_with_TVR(alpha=1.0, no_opt_steps=10)
+    # Export
+    params_traj.export("cache_params.txt")
 
-# Export
-paramsTE_traj.export("cache_derivs.txt")
+else:
+
+    # Import params traj
+    params_traj = ParamsTraj.fromFile("cache_params.txt", nv=2, nh=1)
+
+if False:
+
+    # Differentiate
+    paramsTE_traj = params_traj.differentiate_with_TVR(alpha=1.0, no_opt_steps=10)
+
+    # Export
+    paramsTE_traj.export("cache_derivs.txt")
+
+else:
+
+    # Import paramsTE_traj
+    paramsTE_traj = ParamsTETraj.fromFile("cache_derivs.txt", nv=2, nh=1)
+
+# Freqs, coffs for fourier
+nv = 2
+nh = 1
+freqs = np.random.rand(3)
+muh_sin_coeffs_init = np.random.rand(3)
+muh_cos_coeffs_init = np.random.rand(3)
+varh_sin_coeffs_init = np.random.rand(3)
+varh_cos_coeffs_init = np.random.rand(3)
+
+# Rxns
+rxn_specs = [
+    (RxnSpec.BIRTH,0),
+    (RxnSpec.DEATH,1),
+    (RxnSpec.EAT,2,1)
+    ]
+
+# Reaction input layer
+rxn_lyr = RxnInputsLayer(
+    nv=nv,
+    nh=nh,
+    freqs=freqs,
+    muh_sin_coeffs_init=muh_sin_coeffs_init,
+    muh_cos_coeffs_init=muh_cos_coeffs_init,
+    varh_sin_coeffs_init=varh_sin_coeffs_init,
+    varh_cos_coeffs_init=varh_cos_coeffs_init,
+    rxn_specs=rxn_specs
+    )
+
+size_wt = nv*nh
+size_b = nv
+size_sig2 = 1
+no_outputs = size_wt + size_b + size_sig2
+
+model = tf.keras.models.Sequential([
+    rxn_lyr,
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(no_outputs, activation='relu')
+  ])
